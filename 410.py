f3500503d3a5f32ddb26130cd9c8b80fa891b0af
dibery <dibery@users.noreply.github.com>
2022-03-31 12:06:52 +0800

    Update 410.py

--- 410.py
+++ 410.py
@@ -3,16 +3,15 @@ class Solution:
         l, h = max(nums), sum(nums)
         while l < h:
             t = (l + h) // 2
-            seg = [0]
+            seg, _sum = 1, 0
             for n in nums:
-                if seg[-1] + n <= t:
-                    seg[-1] += n
-                elif len(seg) <= m:
-                    seg.append(n)
+                if _sum + n <= t:
+                    _sum += n
                 else:
+                    seg, _sum = seg + 1, n
+                if seg > m:
+                    l = t + 1
                     break
-            if len(seg) > m:
-                l = t + 1
-            else:
+            if seg <= m:
                 h = t
         return l
