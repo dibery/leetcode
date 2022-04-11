383473bb27b43140ebf6cc53b8d5794d0f890292
dibery <dibery@users.noreply.github.com>
2022-04-11 22:29:39 +0800

    Daily 220411

--- 1260.py
+++ 1260.py
@@ -1,9 +1,5 @@
 import numpy
-
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-        R, C = len(grid), len(grid[0])
-        k %= R * C
-        grid = numpy.array(grid).flatten().tolist()
-        grid = numpy.array(grid[-k:] + grid[:-k])
-        return grid.reshape(R, C)
+        grid = numpy.array(grid)
+        return numpy.roll(grid.flatten(), k).reshape(grid.shape)
