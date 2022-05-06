56df26ece0d264308bce31b93bc86f23a919fd4c
dibery <dibery@users.noreply.github.com>
2022-05-06 09:19:09 +0800

    Daily 220506

--- 1209.py
+++ 1209.py
@@ -1,7 +1,11 @@
-import re
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        pattern = '(.)' + '\\1' * (k - 1)
-        while re.search(pattern, s):
-            s = re.sub(pattern, '', s)
-        return s
+        cnt = [['', 0]]
+        for i in s:
+            if i != cnt[-1][0]:
+                cnt.append([i, 1])
+            else:
+                cnt[-1][1] += 1
+            if cnt[-1][1] == k:
+                cnt.pop()
+        return ''.join(i[0] * i[1] for i in cnt)
